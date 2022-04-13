609f7e9373d3852de4b37c350910416999e1ed47
Sincerelyurs <101644374+Sincerelyurs@users.noreply.github.com>
2022-04-13 17:56:52 +0800

    fourth

--- src/backend/CourseEvalSys/template/models.py
+++ src/backend/CourseEvalSys/template/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Users(models.Model):
     User_ID = models.IntegerField('User_ID',db_column='User_ID',primary_key=True)
-    User_name = models.CharField('User_name',db_column='User_name',max_length=255,null=False)
+    User_name = models.CharField('User_name',db_column='User_name',max_length=255,null=False,unique=True)
     Password = models.CharField('Password',db_column='Password',max_length=255,null=False)
     class Meta:
         db_table = 'Users'
 
 class Courses(models.Model):
     Course_ID = models.IntegerField('Course_ID',db_column='Course_ID',primary_key=True)
-    Course_Name = models.CharField('Course_name',db_column='Course_Name',max_length=255,null=False)
+    Course_Name = models.CharField('Course_name',db_column='Course_Name',max_length=255,null=False,unique=True)
     Department = models.CharField('Department',db_column='Department',max_length=255,null=False)
     Is_Valid = models.BooleanField('Is_Valid',db_column='Is_Valid',null=False) #0:not valid, 1:valid
     Final_Score = models.FloatField('FinalScore',db_column='Final_Score',null=True) #average score?
@@ -31,11 +31,18 @@ class Comments(models.Model):
         db_table = 'Comments'
 
 class CommentsLikeStatus(models.Model):
+    # Status_ID = models.AutoField(primary_key=True,default=1)
+    Status_ID = models.IntegerField('Status_ID',db_column='Status_ID',primary_key=True,null=False,default=1)
     User_ID = models.ForeignKey('Users',to_field='User_ID',related_name='UserID',on_delete=models.CASCADE)
-    CommentID = models.OneToOneField(Comments, related_name='ID_of_Comment',primary_key=True,on_delete=models.CASCADE) # 0:dislike, 1:like
+    CommentID = models.ForeignKey(Comments, related_name='ID_of_Comment',on_delete=models.CASCADE) # 0:dislike, 1:like
     Status = models.BooleanField('Status',db_column='Status',null=False)
     class Meta:
         db_table = 'CommentsLikeStatus'
+        constraints = [
+            models.UniqueConstraint(fields=['User_ID', 'CommentID'], name='like_id'),
+        ]
+
+
 
 class MultiComment(models.Model):
     Multi_Comment_ID = models.IntegerField('Multi_Comment_ID',db_column='Multi_Comment_ID',primary_key=True,null=False)
