41a9273ce409fe2fa64e6b028cc8eacc99962eab
Amehi <676918986@qq.com>
2022-04-29 10:42:21 +0000

    init Login part

---
+++ frontend/src/store/store.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate"
+
+Vue.use(Vuex)
+
+const state = {
+    userID: "",
+    userEmail:"",
+    userNickName: "Unknown",
+    avatar:"",
+    userIsOrganization:false,
+      msg: '123456',
+    hasLogin: false,
+  }
+  
+  const mutations = {
+      loginUpdate (state) {
+      state.hasLogin = true
+    },
+    logoutUpdate (state) {
+      state.hasLogin = false
+    },
+    userIDUpdate(state, id){
+      state.userID = id
+    },
+    userEmailUpdate (state, useremail){
+      state.userEmail = useremail
+    },
+    userNicknameUpdate (state, nickname){
+      state.userNickName = nickname
+    },
+    userAvatarUpdate (state, avatar_url){
+      state.avatar = avatar_url
+    },
+    userIsOrganizationUpdate(state, is_organization){
+      state.userIsOrganization = is_organization
+    }
+  }
+  
+  const actions = {
+      
+  }
+  
+  export default new Vuex.Store({
+      state,
+      actions,
+      mutations,
+    plugins: [createPersistedState()]
+  })
