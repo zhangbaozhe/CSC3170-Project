67be460c507a47c581b8421c40146961dfb4da74
Baozhe Zhang <907711755@qq.com>
2022-04-30 04:02:19 +0000

    added frontend/ from login-lyy; built OK

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+  <h1>Welcom!</h1>
+  <router-view></router-view>
+  </div>
+    
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
