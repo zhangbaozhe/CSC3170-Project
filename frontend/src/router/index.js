18dfc4e99d75ce79ed9ca61c030cbeed4cac8e5f
Baozhe Zhang <907711755@qq.com>
2022-05-05 11:48:38 +0000

    fixed logic bugs in whitelist

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -55,28 +55,17 @@ const router = new VueRouter({
   routes
 })
 
-const whiteList = ["login", "signup"]; // 定义一个白名单列表
+const whiteList = ["/login", "/signup"]; // 定义一个白名单列表
 
 router.beforeEach(async (to, from, next) => {
   var isTokenAvailable;
   isTokenAvailable = store.state.hasLogin; // 校验token是否失效
-  if (!isTokenAvailable && whiteList.includes(to.path)) { // 如果是访问的白名单中的页面
-    return next(); // 不需要校验，直接返回继续访问该页面
+  console.log(to.path)
+  if (!isTokenAvailable && !whiteList.includes(to.path)) { // 如果是访问的白名单中的页面
+    return next("/login"); // 不需要校验，直接返回继续访问该页面
   }
-  if (isTokenAvailable) { // 如果token未失效
-    if(whiteList.includes(to.path)) { // 如果访问的是login页面，则回到首页
-      next("/");
-    } else { // 如果访问的不是login页面，则继续访问当前要访问的页面
-      next();
-    }
-  } else { // 如果token失效了
-    // const needLogin = to.matched.some(item => item.meta.needLogin); // 检测要访问的页面是否需要登录才能访问
-    // if(needLogin) { // 如果访问的页面是需要登录的
-    //   next("/login"); // 跳转到登录页面
-    // } else { // 如果访问的页面是不需要登录的，则直接继续访问
-    //   next();
-    next("/login");
-    // }
+  else {
+    return next()
   }
 });
 export default router
