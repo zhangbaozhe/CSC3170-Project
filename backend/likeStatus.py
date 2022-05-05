2c242abab18606e55f424fd60fbd7a058c716255
Amehi <676918986@qq.com>
2022-05-05 12:49:31 +0000

    complete like/dislike function

--- backend/likeStatus.py
+++ backend/likeStatus.py
@@ -8,9 +8,49 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt
 
+def generate_response(data, status):
+    response = JsonResponse(data, safe=False, status = status)
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "*"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
+
 @csrf_exempt
 @api_view(['GET', 'POST'])
 def like(request):
     with connection.cursor() as cursor:
         if(request.method == "POST"):
-            pass
+            try:
+                userID = request.data["userID"]
+                status = request.data["status"]
+                commentID = request.data["commentID"]
+            except:
+                return generate_response(None, 400)
+            cursor.execute(
+                """
+                INSERT INTO `CommentLikeStatus` 
+                (`UserID`, `CommentID`, `Status`)
+                VALUES
+                (%s, %s, %s);
+                """%(userID, commentID, status)
+            )
+            #TODO: update `UsersSetCommentLikeStatus`?
+            return generate_response(None, 201)
+        elif(request.method == "PUT"):
+            try:
+                userID = request.data["userID"]
+                status = request.data["status"]
+                commentID = request.data["commentID"]
+            except:
+                return generate_response(None, 400)
+            try:
+                cursor.execute(
+                    """
+                    UPDATE `CommentLikeStatus` 
+                    SET `Status` = %s
+                    WHERE `UserID` = %s and `CommentID` = %s;
+                    """%(status, userID, commentID)
+                )
+            except:
+                return generate_response(None, 400)
