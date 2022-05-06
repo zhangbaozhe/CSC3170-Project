61c95e8424867ba1f8666f76b64d653a0c245977
Amehi <676918986@qq.com>
2022-05-06 14:04:36 +0000

    update admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -18,26 +18,156 @@ def generate_response(data, status):
     response["Access-Control-Allow-Headers"] = "*"
     return response
 #allowed: delete,
+@csrf_exempt
+@api_view(['DELETE'])
 def manage_user(request):
     with connection.cursor() as cursor:
         if(request.method == "DELETE"):
             try: 
-                userID = request.data["data"]["id"]
+                userID = str(request.data["data"]["id"])
             except:
                 return generate_response(None, 400)
             try:
                 cursor.execute(
-                    """
+                    f"""
                     DELETE FROM `Users`
-                    WHERE `UserID` = %s;
-                    """%str(userID)
+                    WHERE `UserID` = {userID};
+
+                    DELETE FROM `UsersGiveComments`
+                    WHERE `UserID` = {userID};
+
+                    DELETE FROM `Comments`
+                    WHERE `UserID` = {userID};
+
+                    DELETE FROM `CommentLikeStatusAppraiseComments`
+                    WHERE `UserID` = {userID};
+
+                    DELETE FROM `CommentLikeStatus`
+                    WHERE `UserID` = {userID};
+
+                    DELETE FROM `MultiComments`
+                    WHERE `UserID` = {userID};
+                    """
                 )
             except:
                 return generate_response(None, 400)
             return (None, 204)
 
 #allowed: delete
-#def manage_comment(request):
+@csrf_exempt
+@api_view(['DELETE'])
+def manage_comment(request):
+    with connection.cursor() as cursor:
+        if(request.method == "DELETE"):
+            try: 
+                commentID = str(request.data["data"]["id"])
+            except:
+                return generate_response(None, 400)
+            try:
+                cursor.execute(
+                    f"""
+                    DELETE FROM `Comments`
+                    WHERE `CommentID` = {commentID};
+                    
+                    DELETE FROM `CommentLikeStatusAppraiseComments`
+                    WHERE `CommentID` = {commentID};
+
+                    DELETE FROM `CommentLikeStatus`
+                    WHERE `CommentID` = {commentID};
+
+                    DELETE FROM `UsersSetCommentLikeStatus`
+                    WHERE `CommentID` = {commentID};
+
+                    DELETE FROM `MultiComments`
+                    WHERE `ParentCommentID` = {commentID};
+
+                    DELETE FROM `MultiCommentsReplyComments`
+                    WHERE `CommentID` = {commentID};
+                    """
+                )
+            except:
+                return generate_response(None, 400)
+            return (None, 204)
 
 #allowed: add, delete
-#def manage_course(request):
+@csrf_exempt
+@api_view(['DELETE', 'POST'])
+def manage_course(request):
+    with connection.cursor() as cursor:
+        if(request.method == "DELETE"):
+            try: 
+                courseID = str(request.data["data"]["id"])
+            except:
+                return generate_response(None, 400)
+
+            cursor.execute(
+                f"""
+                DELETE FROM `Courses`
+                WHERE `CourseID` = {courseID};
+                
+                DELETE FROM `Comments`
+                WHERE `CourseID` = {courseID};
+
+                DELETE FROM `CommentsEvaluateCourses`
+                WHERE `CourseID` = {courseID};
+                """
+            )
+
+            return generate_response(None, 204)
+        elif(request.method == "POST"):
+            try: 
+                courseName = request.data["CourseName"]
+                school = request.data['School']
+                credit = request.data["Credits"]
+            except:
+                return generate_response(None, 400)
+            cursor.execute(
+                f"""
+                INSERT INTO `Courses` (
+                    `CourseName`, `School`, `Credits`, `IsValid`, 
+                    `FinalScore`) 
+                    VALUES (
+                    "{courseName}", "{school}", {credit}, 1, 0.0);
+                """
+            )
+            return generate_response(None, 201)
+
+@csrf_exempt
+@api_view(['GET'])
+def get_all(request):
+    with connection.cursor() as cursor:
+        tmp = {}
+        tmp["Courses"] = []
+        tmp["Users"] = []
+        tmp["Comments"] = []
+        cursor.execute(
+            """
+            SELECT `CourseID`, `CourseName` FROM `Courses`
+            """
+        )
+        columns = [col[0] for col in cursor.description]
+        data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        for item in data:
+            tmp["Courses"].append(item)
+
+        cursor.execute(
+            """
+            SELECT `CommentID`, `Content`, `UserID` FROM `Comments`
+            """
+        )
+        columns = [col[0] for col in cursor.description]
+        data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        for item in data:
+            tmp["Comments"].append(item)
+        
+        cursor.execute(
+            """
+            SELECT `UserID`, `UserName` FROM `Users`
+            """
+        )
+        columns = [col[0] for col in cursor.description]
+        data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        for item in data:
+            tmp["Users"].append(item)
+        
+        return generate_response(tmp, 200)
